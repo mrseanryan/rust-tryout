ad1b258482d045080975cd62e3ca2ca36845b118
sean <sean.ryan@mendix.com>
2019-08-09 15:26:49 +0200

    Add project and scripts using cargo

---
+++ src/projects/hello_cargo/go.sh
@@ -0,0 +1,14 @@
+echo cargo check
+cargo check
+
+echo cargo build
+cargo build
+
+echo ./target/debug/hello_cargo
+./target/debug/hello_cargo
+
+echo cargo run
+cargo run
+
+echo cargo build --release
+cargo build --release
