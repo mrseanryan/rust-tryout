02d0878a8ca30c2b8ab8d2b7155dede562f2a2a3
sean <str_ie@yahoo.co.uk>
2019-08-18 11:10:19 +0200

    Refactor guessing game to smaller funs

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,35 +7,54 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 100 + 1);
 
-    // TODO refactor into smaller fns
     loop {
-        println!("Please input your guess");
+        println!("Please input your guess [1..100]");
 
-        // mutable, on the heap (not 's'tring)
-        let mut guess = String::new();
-
-        // passing mutable ref
-        io::stdin()
-            .read_line(&mut guess)
-            // arg to expect() is the error message to show:
-            .expect("Failed to read line");
-
-        // using match instead of expect, to handle the error (not just throw)
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            // _ is a catchall pattern
-            Err(_) => continue,
-        };
+        let guess = get_next_guess();
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        let is_done = is_guess_correct(guess, secret_number);
+
+        if is_done {
+            break;
+        }
+    }
+}
+
+fn get_next_guess() -> i32 {
+    // mutable, on the heap (not 's'tring)
+    let mut guess = String::new();
+
+    // passing mutable ref
+    io::stdin()
+        .read_line(&mut guess)
+        // arg to expect() is the error message to show:
+        .expect("Failed to read line");
+
+    // using match instead of expect, to handle the error (not just throw)
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        // _ is a catchall pattern
+        Err(_) => -1,
+    };
+
+    return guess;
+}
+
+fn is_guess_correct(guess: i32, secret_number: i32) -> bool {
+    match guess.cmp(&secret_number) {
+        Ordering::Less => {
+            println!("Too small!");
+            false
+        }
+        Ordering::Greater => {
+            println!("Too big!");
+            false
+        }
+        Ordering::Equal => {
+            println!("You win!");
+            true
         }
     }
 }
