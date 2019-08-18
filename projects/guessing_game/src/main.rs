cb87d785d4683cd95ab8a0765512453b2d3bdb78
sean <str_ie@yahoo.co.uk>
2019-08-18 10:51:33 +0200

    Improve the guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,9 +1,15 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the secret number");
 
     println!("Please input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1, 100 + 1);
+
+    println!("The secret number is: {}", secret_number);
 
     // mutable, on the heap (not 's'tring)
     let mut guess = String::new();
@@ -12,5 +18,15 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        // arg to expect() is the error message to show:
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
