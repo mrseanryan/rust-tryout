10531e0100747e9849bbebeb003d725c58f0457f
sean <str_ie@yahoo.co.uk>
2019-08-18 11:01:40 +0200

    ch2 code complete

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -5,28 +5,37 @@ use std::io;
 fn main() {
     println!("Guess the secret number");
 
-    println!("Please input your guess");
-
     let secret_number = rand::thread_rng().gen_range(1, 100 + 1);
 
-    println!("The secret number is: {}", secret_number);
+    // TODO refactor into smaller fns
+    loop {
+        println!("Please input your guess");
 
-    // mutable, on the heap (not 's'tring)
-    let mut guess = String::new();
+        // mutable, on the heap (not 's'tring)
+        let mut guess = String::new();
 
-    // passing mutable ref
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // passing mutable ref
+        io::stdin()
+            .read_line(&mut guess)
+            // arg to expect() is the error message to show:
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        // arg to expect() is the error message to show:
-        .expect("Please type a number!");
+        // using match instead of expect, to handle the error (not just throw)
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // _ is a catchall pattern
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
